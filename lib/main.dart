49ed5018a537bf5dfdca5cdacfd75ba8f3a76a7b
JohnCarloHipolito <Johncarlo.Hipolito@triosstudent.com>
2024-08-08 09:08:39 -0400

    clean home

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Word Guess',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
         useMaterial3: true,
       ),
       home: const SplashScreen(
-        nextPage: HomePage(title: 'Flutter Demo Home Page'),
+        nextPage: HomePage(),
       ),
     );
   }
